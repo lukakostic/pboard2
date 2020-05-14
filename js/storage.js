0009d63c360ea4773d347012cf8a19288958d2cf
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2020-05-14 08:47:30 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -65,14 +65,16 @@ let storage = {
 
 
                 var fileBlob = new Blob([file.body], {type: 'text/plain'});
+                /*
                 var metadata = {
                     'name': file.name, // Filename at Google Drive
                     'mimeType': file.mimeType // mimeType at Google Drive
                 };
+                */
       
                 var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
                 var form = new FormData();
-                form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+                //form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
                 form.append('file', fileBlob);
       
                 var xhr = new XMLHttpRequest();
