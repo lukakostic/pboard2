03647070ba9b8b268af5ef005a88233e0f811a95
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2020-05-14 03:36:52 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -37,26 +37,26 @@ let storage = {
 
 
 
-var file = new Blob([file.body], {type: 'text/plain'});
-var metadata = {
-    'name': file.name, // Filename at Google Drive
-    'mimeType': file.mimeType // mimeType at Google Drive
-};
+          var fileBlob = new Blob([file.body], {type: 'text/plain'});
+          var metadata = {
+              'name': file.name, // Filename at Google Drive
+              'mimeType': file.mimeType // mimeType at Google Drive
+          };
 
-var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-var form = new FormData();
-form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-form.append('file', file);
+          var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+          var form = new FormData();
+          form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+          form.append('file', fileBlob);
 
-var xhr = new XMLHttpRequest();
-xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
-xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-xhr.responseType = 'json';
-xhr.onload = () => {
-    console.log(xhr.response); // Retrieve uploaded file ID.
-    if(callback) callback(xhr.response)
-};
-xhr.send(form);
+          var xhr = new XMLHttpRequest();
+          xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+          xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+          xhr.responseType = 'json';
+          xhr.onload = () => {
+              console.log(xhr.response); // Retrieve uploaded file ID.
+              if(callback) callback(xhr.response)
+          };
+          xhr.send(form);
         
 /*
         gapi.client.drive.files.create({
