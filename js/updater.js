c66dabb5e443b723830332618b9b7593fd31a414
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2020-05-15 04:24:21 +0200

    Update updater.js

--- js/updater.js
+++ js/updater.js
@@ -2,18 +2,17 @@ let curVer = 2
 
 function copyNewProperties(from, to){
     let fields = Object.keys(from)
-    for(let i = 0; i < fields.length; i++){
-        if((fields[i] in to) == false){
+    for(let i = 0; i < fields.length; i++)
+        if((fields[i] in to) == false)
             to[fields[i]] = from[fields[i]]
-        }
-    }
+        
     return to
 }
 
 function updateProject(proj){
-    if(proj.version == curVer){
+    if(proj.version == curVer)
         return proj
-    }
+    
     if(proj.version == 1){
         delete proj.preferences['manualSaveLoad']
         let pref = copyNewProperties(new Project().preferences,proj.preferences)
